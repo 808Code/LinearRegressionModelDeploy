19ddfffb78e49dc0c70e40eb983b9cce73b4f316
808Code <dikshant.shah2k@gmail.com>
2022-11-28 19:57:01 +0545

    Model form created

--- smoke_opacity/predict/forms.py
+++ smoke_opacity/predict/forms.py
@@ -2,6 +2,5 @@ from django import forms
 
 
 class PreditctForm(forms.Form):
-    loan_amnt = forms.IntegerField(min_value=0)
-    term = forms.IntegerField(min_value=0)
-    int_rate = forms.FloatField(min_value=0)
+    km = forms.IntegerField(min_value=30000,max_value=400000,label="Distance covered in KM")
+    manufacture_date = forms.IntegerField(min_value=2000,max_value=2020,label="Manufacture date")
--- smoke_opacity/predict/views.py
+++ smoke_opacity/predict/views.py
@@ -5,6 +5,6 @@ from .forms import PreditctForm
 
 # Create your views here.
 def index(request):
-    form = PreditctForm()
+    form = PreditctForm({'km':30000,'manufacture_date':2000})
     context = {'form': form}
     return render(request, 'predict/index.html', context)
