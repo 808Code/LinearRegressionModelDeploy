fdde57e5766cdf5f4cdcf4909f885e0a97e28663
808Code <dikshant.shah2k@gmail.com>
2022-11-29 19:31:02 +0545

    model deployed

--- smoke_opacity/predict/forms.py
+++ smoke_opacity/predict/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class PreditctForm(forms.Form):
-    km = forms.IntegerField(min_value=30000,max_value=400000,label="Distance covered in KM")
-    manufacture_date = forms.IntegerField(min_value=2000,max_value=2020,label="Manufacture date")
+    km = forms.IntegerField(min_value=38920,max_value=400000,label="Distance covered in KM")
+    manufacture_date = forms.IntegerField(min_value=2006,max_value=2020,label="Manufacture date")
--- smoke_opacity/predict/views.py
+++ smoke_opacity/predict/views.py
@@ -1,10 +1,30 @@
+import pickle
+
+import pandas as pd
 from django.shortcuts import render
 
 from .forms import PreditctForm
 
+filename = 'finalized_model.sav'
+loaded_model = pickle.load(open(filename, 'rb'))
+test_data = pd.DataFrame(columns=["KM", "Man. Date"])
+
 
 # Create your views here.
 def index(request):
-    form = PreditctForm({'km':30000,'manufacture_date':2000})
-    context = {'form': form}
+    context={}
+    if request.method == 'GET':
+        form = PreditctForm({'km': 38920, 'manufacture_date': 2006})
+        context = {'form': form}
+    else:
+        d = request.POST.dict()
+        del d['csrfmiddlewaretoken']
+        recieved_values = list(d.values())
+        form = PreditctForm(initial=d)
+        km = int(recieved_values[0])
+        manufacture_date = 2020 - int(recieved_values[1])
+        test_data.loc[0] = [km, manufacture_date]
+        result = loaded_model.predict(test_data[["KM", "Man. Date"]].to_numpy())[0][0]
+        context['form'] = form
+        context['prediction'] = round(result, 2)
     return render(request, 'predict/index.html', context)
